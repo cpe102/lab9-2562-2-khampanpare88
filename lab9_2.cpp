5cd0b5b87e8078f6bb808fe479067d777e0bf0f3
lab302-8 <lab302-8@labeng.local>
2020-01-28 16:40:32 +0700

    lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,22 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+ifstream source;
+source.open("cheerbook.txt");
+string textline;
+ofstream dest;
+    dest.open("cheerbook_copy.txt");
+    dest <<"-------------------- SOTUS ---------------------"<<"\n";
+    while (getline(source,textline))
+    {
+      dest << textline << "\n";
+    }
+    dest << "-------------------- SOTUS ---------------------";
+    dest.close();
+    return 0;
+}
+
+
