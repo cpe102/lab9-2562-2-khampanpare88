c67ee7d6ea1a738de181d3ce9399fd399c37801f
lab302-8 <lab302-8@labeng.local>
2020-01-28 16:41:16 +0700

    lab9_3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,32 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum;
+    float x,y,z;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+        sum +=atof(textline.c_str());
+        y=y+pow(atof(textline.c_str()),2);
+        count++;
+    }
+    cout<<"Number of data ="<<count<<"\n";
+    x=sum/count;
+    cout << "Mean = "<< x <<"\n";
+   
+    z=pow((y/count)-pow(x,2),0.5);
+    cout << "Standard deviation = "<< z <<"\n";
+    return 0;
+}
+
+
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
